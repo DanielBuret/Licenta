3ac373a2d67f229123d3109ee6f2ecee46a889f2
Danu <162121252+DanielBuret@users.noreply.github.com>
2026-05-03 19:19:23 +0300

    feat(frontend): add login page

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { LoginPage } from '../pages/LoginPage';
 
 export const router = createBrowserRouter([
   {
@@ -7,7 +8,7 @@ export const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <div>LoginPage placeholder</div>,
+    element: <LoginPage />,
   },
   {
     path: '/register',
